125ff3d185f2776a1a39e77358ab70ac41b8c293
owen <thss15_zhangqr@163.com>
2017-09-30 20:56:16 +0800

    add week 2's homework

--- week2/arraySumLoop.asm
+++ week2/arraySumLoop.asm
@@ -1,5 +1,5 @@
 ;2017.9.28
-;int array sum without loop
+;int array sum with loop
 ;to run this, Irvine32.inc need to be in your path
 ;By Owen6314
 ;--------------------------------------------------------------------------------------
@@ -11,22 +11,21 @@ ExitProcess PROTO, dwExitCode:DWORD
 
 .data
 	intArray WORD 600h,500h,400h,300h,200h
-	sum DWORD 0h
+	sum WORD 0h
 .code
 main PROC
     mov eax,0
-    movzx ebx, intArray
-    add eax, ebx
-    movzx ebx, intArray + 2
-    add eax, ebx
-    movzx ebx, intArray + 4
-    add eax, ebx
-    movzx ebx, intArray + 6
-    add eax, ebx
-    movzx ebx, intArray + 8
-    add eax, ebx
-	mov sum, eax
-    call WaitMsg		
+	mov ebx,OFFSET intArray
+	mov ecx,LENGTHOF intArray
+L1:
+	mov eax, [ebx]
+	add sum,ax
+	add ebx,TYPE intArray
+	loop L1	
+
+	movzx eax, sum
+	call WriteInt
+	call WaitMsg
 	INVOKE ExitProcess,0
 main ENDP
 END main
--- week2/arraySumNoLoop.asm
+++ week2/arraySumNoLoop.asm
@@ -1,5 +1,5 @@
 ;2017.9.28
-;int array sum with loop
+;int array sum without loop
 ;to run this, Irvine32.inc need to be in your path
 ;By Owen6314
 ;--------------------------------------------------------------------------------------
@@ -11,17 +11,23 @@ ExitProcess PROTO, dwExitCode:DWORD
 
 .data
 	intArray WORD 600h,500h,400h,300h,200h
-	sum WORD 0h
+	sum DWORD 0h
 .code
 main PROC
     mov eax,0
-	mov ebx,OFFSET intArray
-	mov ecx,LENGTHOF intArray
-L1:
-	mov eax, [ebx]
-	add sum,ax
-	add ebx,TYPE intArray
-	loop L1	
+    movzx ebx, intArray
+    add eax, ebx
+    movzx ebx, intArray + 2
+    add eax, ebx
+    movzx ebx, intArray + 4
+    add eax, ebx
+    movzx ebx, intArray + 6
+    add eax, ebx
+    movzx ebx, intArray + 8
+    add eax, ebx
+	mov sum, eax
+	call WriteInt
+    call WaitMsg		
 	INVOKE ExitProcess,0
 main ENDP
 END main
